6f90589ca0e989dd1a2738004605006675aa0d35
alpellegri <alpellegri@gmail.com>
2016-09-17 13:15:48 +0200

    added config file for esp8266

--- esp8266/src/fcm_config.cpp
+++ esp8266/src/fcm_config.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
 
-String FCM_AUTH = "AIza***********************************";
+String FCM_AUTH = "AIzaS**********************************";
 char FcmServer[50] = "fcm.googleapis.com";
 char TimeServer[50] = "google.com";
