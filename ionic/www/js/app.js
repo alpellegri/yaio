8a853f904d66f9d25fa8b035ea42677095d2ecfd
alpellegri <alpellegri@gmail.com>
2016-12-08 14:15:44 +0100

    ionic framwork upgrade

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -1,6 +1,10 @@
 // Ionic Starter App
 
-angular.module('app', ['ionic', 'chart.js',
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'chart.js',
   'app.services',
   'app.services.firebase',
   'app.services.websocket',
