c97f02e92dca7f06231e4e934add7dca5092ff41
alpellegri <alpellegri@gmail.com>
2017-09-10 17:45:18 +0200

    better versioning

--- esp8266/src/vers.cpp
+++ esp8266/src/vers.cpp
@@ -1,7 +1,10 @@
 #define STRINGIZER(arg) #arg
 #define STR_VALUE(arg) STRINGIZER(arg)
 
-#define VERSION STRINGIZER(1) "." STRINGIZER(4)
+#define MAJOR 1
+#define MINOR 5
+
+#define VERSION STRINGIZER(MAJOR) "." STRINGIZER(MINOR)
 const char *build_str = VERSION " " __DATE__ " " __TIME__;
 
 const char *VERS_getVersion(void) { return build_str; }
