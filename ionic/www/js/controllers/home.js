6161a7e1058c0e2f0ab1583fe12284c33f1738a5
alpellegri <alpellegri@gmail.com>
2017-10-16 19:25:11 +0200

    ionic fix

--- ionic/www/js/controllers/home.js
+++ ionic/www/js/controllers/home.js
@@ -10,6 +10,15 @@ angular.module('app.controllers.home', [])
 
     var fb_init = 'true'; // localStorage.getItem('firebase_init');
     if (fb_init == 'true') {
+
+      $scope.pushCtrl0Change = function() {
+        var ref = firebase.database().ref("control").update({
+          alarm: $scope.control.alarm
+        });
+        var current_date = new Date();
+        firebase.database().ref("control/time").set(Math.floor(current_date.getTime() / 1000));
+      };
+
       $scope.doRefresh = function() {
         console.log('doRefresh-HomeCtrl');
         if (FirebaseService.up() == true) {
