f3d250251704dbc4a5984cba4267311036af882d
alpellegri <alpellegri@gmail.com>
2017-12-28 11:43:38 +0100

    esp8266 lib update

--- esp8266/lib/Adafruit SSD1306_ID135/Adafruit_SSD1306.h
+++ esp8266/lib/Adafruit SSD1306_ID135/Adafruit_SSD1306.h
@@ -32,13 +32,17 @@ All text above, and the splash screen must be included in any redistribution
  #define HAVE_PORTREG
 #elif defined(ARDUINO_ARCH_SAMD)
 // not supported
-#elif defined(ESP8266) || defined(ARDUINO_STM32_FEATHER)
+#elif defined(ESP8266) || defined(ESP32) || defined(ARDUINO_STM32_FEATHER) || defined(__arc__)
   typedef volatile uint32_t PortReg;
   typedef uint32_t PortMask;
-#else
+#elif defined(__AVR__)
   typedef volatile uint8_t PortReg;
   typedef uint8_t PortMask;
- #define HAVE_PORTREG
+  #define HAVE_PORTREG
+#else
+  // chances are its 32 bit so assume that
+  typedef volatile uint32_t PortReg;
+  typedef uint32_t PortMask;
 #endif
 
 #include <SPI.h>
@@ -66,8 +70,8 @@ All text above, and the splash screen must be included in any redistribution
     SSD1306_96_16
 
     -----------------------------------------------------------------------*/
-   #define SSD1306_128_64
-//   #define SSD1306_128_32
+//   #define SSD1306_128_64
+   #define SSD1306_128_32
 //   #define SSD1306_96_16
 /*=========================================================================*/
 
