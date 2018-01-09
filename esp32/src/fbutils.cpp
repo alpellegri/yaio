c70f74709aaedc21ecb8e95530d769f63a594ac1
alpellegri <alpellegri@gmail.com>
2018-01-09 19:31:39 +0100

    fix

--- esp32/src/fbutils.cpp
+++ esp32/src/fbutils.cpp
@@ -104,7 +104,7 @@ void FB_dumpFunctions(void) {
   Serial.println(F("FB_dumpFunctions"));
   for (uint8_t i = 0; i < FunctionVec.size(); ++i) {
     Serial.printf_P(PSTR("%d: %s, %s, %x, %d, %s\n"), i,
-                    IoEntryVec[i].key.c_str(), IoEntryVec[i].name.c_str(),
-                    IoEntryVec[i].code, IoEntryVec[i].cb.c_str());
+                    FunctionVec[i].key.c_str(), FunctionVec[i].name.c_str(),
+                    FunctionVec[i].code, FunctionVec[i].cb.c_str());
   }
 }
