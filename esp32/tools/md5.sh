49f72be1cf033325b3d79da7045d1b32906367bd
alpellegri <alpellegri@gmail.com>
2019-09-03 07:50:36 +0200

    update

--- esp32/tools/md5.sh
+++ esp32/tools/md5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-file=../.pioenvs/featheresp32/firmware.bin
+file=../.pio/build/featheresp32/firmware.bin
 cp $file ESP32firmware.bin
 printf $(md5sum ESP32firmware.bin) > ESP32firmware.md5
