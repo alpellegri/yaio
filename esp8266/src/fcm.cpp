ff2bbd56ce4dae0ef0324ab0b8c00b8a1bd8c468
alpellegri <alpellegri@gmail.com>
2019-12-21 10:43:31 +0100

    fix

--- esp8266/src/fcm.cpp
+++ esp8266/src/fcm.cpp
@@ -22,24 +22,24 @@ void FcmSendPush(String &message) {
     }
     String fcm_server_key = EE_GetFirebaseServerKey();
     response = Firebase.sendMessage(message, fcm_server_key, tokens);
-  }
 
-  DEBUG_PRINT("%s\n", response.c_str());
-  DynamicJsonDocument doc(4096);
-  auto error = deserializeJson(doc, response);
-  if (!error) {
-    JsonObject object = doc.as<JsonObject>();
-    JsonArray results = object[F("results")].as<JsonArray>();
-    String fcmtoken_path = FbGetPath_fcmtoken();
-    for (uint32_t i = 0; i < results.size(); ++i) {
-      if (results[i][F("error")] != nullptr) {
-        String data = results[i][F("error")].as<String>();
-        DEBUG_PRINT("[FCM][%d] data: %s\n", i, data.c_str());
-        // Firebase.remove(data);
-        String path = fcmtoken_path + "/" + ids[i].key;
-        DEBUG_PRINT("remove token: %s\n", path.c_str());
-        FB_clearRegIDsDB(i);
-        Firebase.remove(path);
+    DEBUG_PRINT("%s\n", response.c_str());
+    DynamicJsonDocument doc(4096);
+    auto error = deserializeJson(doc, response);
+    if (!error) {
+      JsonObject object = doc.as<JsonObject>();
+      JsonArray results = object[F("results")].as<JsonArray>();
+      String fcmtoken_path = FbGetPath_fcmtoken();
+      for (uint32_t i = 0; i < results.size(); ++i) {
+        if (results[i][F("error")] != nullptr) {
+          String data = results[i][F("error")].as<String>();
+          DEBUG_PRINT("[FCM][%d] data: %s\n", i, data.c_str());
+          // Firebase.remove(data);
+          String path = fcmtoken_path + "/" + ids[i].key;
+          DEBUG_PRINT("remove token: %s\n", path.c_str());
+          FB_clearRegIDsDB(i);
+          Firebase.remove(path);
+        }
       }
     }
   }
