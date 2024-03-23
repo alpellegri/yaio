e6933394bc7086f38717968f3ee18208bea9e479
alessio pellegrinetti <alessio.pellegrinetti@gmail.com>
2024-03-23 10:41:35 +0100

    update

--- yaio-flutter/lib/main.dart
+++ yaio-flutter/lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'drawer.dart';
 import 'login.dart';
@@ -21,14 +22,38 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/// Define a top-level named handler which background/terminated messages will
-/// call.
+/// Working example of FirebaseMessaging.
+/// Please use this in order to verify messages are working in foreground, background & terminated state.
+/// Setup your app following this guide:
+/// https://firebase.google.com/docs/cloud-messaging/flutter/client#platform-specific_setup_and_requirements):
 ///
-/// To verify things are working, check out the native platform logs.
+/// Once you've completed platform specific requirements, follow these instructions:
+/// 1. Install melos tool by running `flutter pub global activate melos`.
+/// 2. Run `melos bootstrap` in FlutterFire project.
+/// 3. In your terminal, root to ./packages/firebase_messaging/firebase_messaging/example directory.
+/// 4. Run `flutterfire configure` in the example/ directory to setup your app with your Firebase project.
+/// 5. Open `token_monitor.dart` and change `vapidKey` to yours.
+/// 6. Run the app on an actual device for iOS, android is fine to run on an emulator.
+/// 7. Use the following script to send a message to your device: scripts/send-message.js. To run this script,
+///    you will need nodejs installed on your computer. Then the following:
+///     a. Download a service account key (JSON file) from your Firebase console, rename it to "google-services.json" and add to the example/scripts directory.
+///     b. Ensure your device/emulator is running, and run the FirebaseMessaging example app using `flutter run`.
+///     c. Copy the token that is printed in the console and paste it here: https://github.com/firebase/flutterfire/blob/01b4d357e1/packages/firebase_messaging/firebase_messaging/example/lib/main.dart#L32
+///     c. From your terminal, root to example/scripts directory & run `npm install`.
+///     d. Run `npm run send-message` in the example/scripts directory and your app will receive messages in any state; foreground, background, terminated.
+///  Note: Flutter API documentation for receiving messages: https://firebase.google.com/docs/cloud-messaging/flutter/receive
+///  Note: If you find your messages have stopped arriving, it is extremely likely they are being throttled by the platform. iOS in particular
+///  are aggressive with their throttling policy.
+///
+/// To verify that your messages are being received, you ought to see a notification appearon your device/emulator via the flutter_local_notifications plugin.
+/// Define a top-level named handler which background/terminated messages will
+/// call. Be sure to annotate the handler with `@pragma('vm:entry-point')` above the function declaration.
+@pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp();
   print('Handling a background message ${message.messageId}');
 }
 
@@ -38,9 +63,12 @@ late AndroidNotificationChannel channel;
 /// Initialize the [FlutterLocalNotificationsPlugin] package.
 late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
+bool isFlutterLocalNotificationsInitialized = false;
+
 Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // Set the background messaging handler early on, as a named top-level function
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
@@ -69,6 +97,7 @@ Future<void> main() async {
     badge: true,
     sound: true,
   );
+  isFlutterLocalNotificationsInitialized = true;
 
   print('app start');
   DateTime now = DateTime.now();
--- yaio-flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ yaio-flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import firebase_core
 import firebase_database
 import firebase_messaging
 import flutter_local_notifications
+import google_sign_in_ios
 import package_info_plus
 import shared_preferences_foundation
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
